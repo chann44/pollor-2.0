989c61f13e8fdc5611475605272716c6116d1bc3
chann44 <44chansw@gmail.com>
2024-01-18 23:13:17 +0530

    feat: render all the polls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import { Poll } from "@/components/poll";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const data = await db.poll.findMany({
+    include: {
+      Option: true,
+    },
+  });
   return (
-    <main>
-      <h1>hello</h1>
+    <main className="flex flex-col gap-y-5">
+      {data?.map((poll, index) => (
+        <Poll key={index} {...poll} />
+      ))}
     </main>
   );
 }
--- src/components/poll.tsx
+++ src/components/poll.tsx
@@ -1,14 +1,14 @@
 import { Option, Poll } from "@prisma/client";
 
 type PollData = Poll & {
-  options: Option[];
+  Option: Option[];
 };
 
 export function Poll(props: PollData) {
   return (
     <section>
       <h2>{props.title}</h2>
-      {props.options.map((option) => {
+      {props.Option?.map((option) => {
         return <p key={option.id}>{option.text}</p>;
       })}
     </section>
